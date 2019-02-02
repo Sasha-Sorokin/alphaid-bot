6b8a27f519c0fbd369141bb0c929730ea89f95fc
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2019-02-02 13:46:29 +0700

    ModuleLoader:Decorators: add decorators
    Adds experimental decorators to simplify init and unload methods

---
+++ src/types/ModuleLoader/Decorators.ts
@@ -0,0 +1,39 @@
+import { ModuleBase } from "./Interfaces.new";
+import { ErrorMessages } from "@sb-types/Consts";
+import { ModulePrivateInterface } from "./PrivateInterface";
+
+type InitializationFunction<T> = (i?: ModulePrivateInterface<T>) => Promise<void>;
+
+export function initializationMethod<T>(_target: ModuleBase<T>, _key: string, descriptor: TypedPropertyDescriptor<InitializationFunction<T>>) {
+	const originalFunc = descriptor.value;
+
+	if (!originalFunc) { return descriptor; }
+
+	descriptor.value = function (this: ModuleBase<T>, i: ModulePrivateInterface<T>) {
+		if (i.baseCheck(this) && !i.isPendingInitialization()) {
+			throw new Error(ErrorMessages.NOT_PENDING_INITIALIZATION);
+		}
+
+		return originalFunc.call(this, arguments);
+	};
+
+	return descriptor;
+}
+
+type UnloadFunction<T> = (i?: ModulePrivateInterface<T>, reason?: string) => Promise<boolean>;
+
+export function unloadMethod<T>(_target: ModuleBase<T>, _key: string, descriptor: TypedPropertyDescriptor<UnloadFunction<T>>) {
+	const originalFunc = descriptor.value;
+
+	if (!originalFunc) return descriptor;
+
+	descriptor.value = function (this: ModuleBase<T>, i: ModulePrivateInterface<T>) {
+		if (i.baseCheck(this) && !i.isPendingUnload()) {
+			throw new Error(ErrorMessages.NOT_PENDING_UNLOAD);
+		}
+
+		return originalFunc.call(this, arguments);
+	};
+
+	return descriptor;
+}
