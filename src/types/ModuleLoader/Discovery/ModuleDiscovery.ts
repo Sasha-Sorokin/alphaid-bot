02109041d434b993a901bf5f531a28ed62f0f7fe
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2019-03-03 01:34:45 +0700

    feat(ModuleLoader/Discovery): add node_modules discovery

--- src/types/ModuleLoader/Discovery/ModuleDiscovery.ts
+++ src/types/ModuleLoader/Discovery/ModuleDiscovery.ts
@@ -7,6 +7,10 @@ import * as semver from "semver";
 
 const LOG = getLogger("ModuleDiscovery");
 
+const NODE_MODULES_PATH = path.join(process.cwd(), "node_modules");
+
+const SCOPED = /^@[a-z]+\/.+$/;
+
 /**
  * Directory routing
  * @ignore
@@ -22,7 +26,6 @@ export interface IRouting {
  * Searches the modules within the directory
  * @param directory Directory to look up for modules
  * @returns Discovered modules within the subdirectories of directory
- * @ignore
  */
 export async function build(directory: string) {
 	const subdirectories = await discoveryUtils.subdirectories(directory);
@@ -31,9 +34,69 @@ export async function build(directory: string) {
 }
 
 /**
+ * Searches the modules within the root `node_modules` directory
+ * @param prefix Prefix for the directory name to match
+ * @returns Discovered modules within the subdirectories of `node_module` directory
+ */
+export async function buildNodeModules(prefix: string) {
+	const subdirectories = await findNodeModules(undefined, prefix);
+
+	return discover(subdirectories);
+}
+
+/**
+ * Searches the node_modules directories that match specified prefix
+ * @param directory Modules directory to search in
+ * @param prefix Prefix to match a directory
+ * @returs Array of the directories that match the prefix
+ */
+async function findNodeModules(directory = NODE_MODULES_PATH, prefix: string) {
+	const baseSubdirectories = await discoveryUtils.subdirectories(directory);
+
+	const matchedSubdirectories: string[] = [];
+
+	for (let i = 0, l = baseSubdirectories.length; i < l; i++) {
+		const subdirectory = baseSubdirectories[i];
+
+		const dirName = path.basename(subdirectory);
+
+		if (SCOPED.test(dirName)) {
+			matchedSubdirectories.push(
+				...(await findNodeModules(subdirectory, prefix))
+			);
+
+			continue;
+		}
+
+		
+		if (!dirName.startsWith(prefix)) continue;
+
+		LOG("info", `Node Modules: Checking "${subdirectory}"`);
+
+		const nodeModulesInside = await discoveryUtils.exists(
+			path.join(subdirectory, "node_modules"),
+			_ => _.isDirectory()
+		);
+
+		if (nodeModulesInside) {
+			// discover them too!
+
+			matchedSubdirectories.push(
+				...(await findNodeModules(nodeModulesInside, prefix))
+			);
+		}
+
+		matchedSubdirectories.push(subdirectory);
+	}
+
+	return matchedSubdirectories;
+}
+
+/**
  * Discovers modules and directories
  * @param directories Directories to look up for modules and routes
  * @returns Discovered modules within provided directories
+ * @ignore
  */
 async function discover(directories: string[]) {
 	const discovery: IModuleInfo[] = [];
@@ -84,14 +147,67 @@ async function discover(directories: string[]) {
  * Lookups for module in the directory
  * @param directory Directory to look up for module
  * @returns Module within the directory or `undefined`
+ * @ignore
  */
 async function moduleInDirectory(directory: string) {
-	const content = await discoveryUtils.safeRead(path.join(directory, "mod.yml"));
+	const declarationFile = path.join(directory, "mod.yml");
+
+	const content = await discoveryUtils.safeRead(declarationFile);
 
 	if (content == null) return undefined;
 
 	try {
 		const moduleInfo = <IModuleInfo> YAML.safeLoad(content);
+		
+		if (moduleInfo.name == null) {
+			throw new Error(`Module name must be provided. File: ${declarationFile}`);
+		}
+
+		if (moduleInfo.nodeModule != null) {
+			LOG("warn", `Module Search: Modules must not declare "nodeModule" property (in "${moduleInfo.name}")`);
+		}
+
+		moduleInfo.nodeModule = directory.startsWith(NODE_MODULES_PATH);
+
+		if (moduleInfo.nodeModule) {
+			const packageFile = await discoveryUtils.safeRead(path.join(directory, "package.json"));
+
+			if (packageFile == null) {
+				throw new Error(`Modules that are node modules must have "package.json" file (in "${moduleInfo.name}")`);
+			}
+
+			const packageInfo = JSON.parse(packageFile);
+
+			if (moduleInfo.version == null) {
+				moduleInfo.version = packageInfo.version;
+
+				if (moduleInfo.version == null) {
+					throw new Error(`Modules that are node modules must have version declared in either "mod.yml" file or "package.json" (in "${moduleInfo.name}")`);
+				}
+			}
+
+			if (moduleInfo.main == null) {
+				moduleInfo.main = packageInfo.main;
+
+				if (moduleInfo.main == null) {
+					throw new Error(`Modules that are node modules must have main file declared in either "mod.yml" file or "package.json" (in "${moduleInfo.name}")`);
+				}
+			}
+		}
+
+		// Parse and check the version:
+		const parsedVersion = semver.valid(moduleInfo.version);
+		
+		if (!parsedVersion) {
+			throw new Error(`Invalid version - ${moduleInfo.version}. File: ${declarationFile}`);
+		}
+
+		moduleInfo.version = parsedVersion;
+
+		// Check file name and normalize path (just in case):
+		if (moduleInfo.main == null) {
+			throw new Error(`No file to construct specified. File: ${declarationFile}`);
+		}
 
 		const normalizedPath = discoveryUtils.normalizeFileName(moduleInfo.main, directory);
 
@@ -99,13 +215,9 @@ async function moduleInDirectory(directory: string) {
 
 		moduleInfo.main = normalizedPath;
 
-		const parsedVersion = semver.valid(moduleInfo.version);
-
-		if (!parsedVersion) {
-			throw new Error(`Invalid version - ${moduleInfo.version}`);
-		}
-
-		moduleInfo.version = parsedVersion;
+		// Check and set defaults:
+		if (moduleInfo["no-alternatives"] == null) moduleInfo["no-alternatives"] = true;
+		if (moduleInfo.entrypoint == null) moduleInfo.entrypoint = "default";
 
 		return moduleInfo;
 	} catch (err) {
@@ -115,11 +227,14 @@ async function moduleInDirectory(directory: string) {
 	}
 }
 
+// TODO: Extract above module info preparation to an another public (?) function
+
 /**
  * Lookups for route in the directory
  * @param directory Directory to look up for route
  * @throws Throws an error if one of the pathes does not belong to the directory
  * @returns Routing within the directory or `undefined`
+ * @ignore
  */
 async function routingInDirectory(directory: string) {
 	const content = await discoveryUtils.safeRead(path.join(directory, "routes.yml"));
