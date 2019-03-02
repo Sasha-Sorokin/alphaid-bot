cf66b44386f018c1b4265408d8682b6df46f5ae7
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2019-03-03 01:36:36 +0700

    feat(ModuleLoader): add support for node_modules
    - Add support for discovering and initializing node modules
    - Also adds support for having multiple instances with same name

--- src/types/ModuleLoader/ModuleLoader.new.ts
+++ src/types/ModuleLoader/ModuleLoader.new.ts
@@ -1,4 +1,4 @@
-import { INullableHashMap } from "@sb-types/Types";
+import { INullableHashMap, IHashMap } from "@sb-types/Types";
 import * as logger from "loggy";
 import * as path from "path";
 import * as Interfaces from "@sb-types/ModuleLoader/Interfaces.new";
@@ -27,6 +27,11 @@ export interface IModuleLoaderConfig {
 	 * Directory to prototype modules from
 	 */
 	modulesPath: string;
+
+	/**
+	 * Resolves npm modules having the following in the directory name
+	 */
+	nodeModulesPrefix?: string;
 }
 
 // #endregion
@@ -57,12 +62,19 @@ export class ModuleLoader extends EventEmitter {
 	/**
 	 * Informations about the modules
 	 */
-	private readonly _infos: INullableHashMap<Interfaces.IModuleInfo> = Object.create(null);
+	private readonly _infos: INullableHashMap<Interfaces.IModuleInfo[]> = Object.create(null);
 
 	/**
 	 * Keepers for modules
 	 */
-	private readonly _keepers: INullableHashMap<ModuleKeeper<any>> = Object.create(null);
+	private readonly _keepers: INullableHashMap<Array<ModuleKeeper<any>>> = Object.create(null);
+
+	/**
+	 * Keepers mapped by the module information
+	 */
+	private readonly _infoMappedKeepers = new WeakMap<Interfaces.IModuleInfo, ModuleKeeper<any>>();
+
+	private readonly _allKeepers: Array<ModuleKeeper<any>> = [];
 
 	/**
 	 * Logger function
@@ -93,12 +105,21 @@ export class ModuleLoader extends EventEmitter {
 	public async rebuildRegistry() {
 		this._log("verb", "Rebuilding registry...");
 
-		const modules = await ModuleDiscovery.build(this.config.modulesPath);
+		let modules = await ModuleDiscovery.build(this.config.modulesPath);
 
-		const checked: Interfaces.IModuleInfo[] = [];
-		const takenNames: string[] = [];
+		const { nodeModulesPrefix } = this.config;
+
+		if (nodeModulesPrefix != null) {
+			this._log("info", `Enabled loading of modules from node_modules directory with prefix: ${nodeModulesPrefix}`);
+
+			modules = modules.concat(
+				await ModuleDiscovery.buildNodeModules(nodeModulesPrefix)
+			);
+		}
 
 		const modNameRegexp = ModuleLoader.moduleNameRegexp;
+
+		const discovered: INullableHashMap<Interfaces.IModuleInfo[]> = Object.create(null);
 
 		for (let i = 0, l = modules.length; i < l; i++) {
 			const mod = modules[i];
@@ -107,69 +128,124 @@ export class ModuleLoader extends EventEmitter {
 				throw new Error(`Incorrect name - "${mod.name}"`);
 			}
 
-			if (takenNames.includes(mod.name)) {
-				throw new Error(`The name "${mod.name}" is taken already`);
-			}
-
 			if (this.config.disabled.includes(mod.name)) {
-				this._log("verb", `Module "${mod.name}" is disabled`);
+				this._log("verb", `Modules with name "${mod.name}" are disabled`);
 
 				continue;
 			}
 
-			takenNames.push(mod.name);
+			if (!(mod.name in discovered)) {
+				discovered[mod.name] = [mod];
 
-			checked.push(mod);
+				continue;
+			}
+
+			const alternatives = discovered[mod.name];
+
+			if (alternatives == null) {
+				throw new Error(`Incorrect state has occured: discovered["${mod.name}"] supposed to be not defined at all, not set to ${alternatives}`);
+			}
+
+			// mod@0.3.2 -A <- allowed
+			// mod@0.6.1 NA
+			// mod@0.6.2 NA
+			// mod@0.7.3 NA <- newest, allowed (highest of non-alternative ones)
+
+			let skip = false;
+
+			// Do not allow same versions
+
+			for (let i = 0, l = alternatives.length; i < l; i++) {
+				const alternative = alternatives[i];
+			
+				if (alternative.version !== mod.version) continue;
+
+				skip = true;
+
+				break;
+			}
+
+			if (skip) {
+				this._log("info", `Module "${mod.name}" with version "${mod.version}" is already found ("${mod.main}")`);
+
+				continue;
+			}
+
+			alternatives.push(mod);
 		}
 
 		let logStr = "";
 
-		const readyCount = checked.length;
+		let readyCount = 0;
 
-		for (let i = 0; i < readyCount; i++) {
-			const modInfo = checked[i];
-		
-			const current = this._infos[modInfo.name];
+		for (const name in discovered) {
+			let discovery = discovered[name]!;
 
-			if (current) {
-				this._checkAndUpdate(current, modInfo);
-			} else {
-				this._infos[modInfo.name] = modInfo;
+			const nonAlternative = discovery.filter(_ => _["no-alternatives"]);
+
+			if (nonAlternative.length > 1) {
+				let highest: Interfaces.IModuleInfo = nonAlternative[0];
+
+				for (let i = 0, l = discovery.length; i < l; i++) {
+					const alternative = discovery[i];
+				
+					if (!alternative["no-alternatives"]) continue;
+
+					if (semver.lte(alternative.version, highest.version)) continue;
+
+					highest = alternative;
+				}
+
+				discovery = discovery.filter(_ => !_["no-alternatives"]);
+
+				discovery.push(highest);
 			}
 
-			logStr += `\n\t- ${modInfo.name}@${modInfo.version} - ${modInfo.main}`;
+			// tslint:disable-next-line: prefer-template
+			logStr += "\n\t" + discovery.map(_ => {
+				let entry = `${_.name}@${_.version}`;
+
+				if (_["no-alternatives"]) entry += " *";
+
+				return entry;
+			}).join("\n\t");
+
+			readyCount += discovery.length;
+
+			this._infos[name] = discovery;
 		}
 
 		this._log("verb", `Ready ${readyCount} modules:${logStr}`);
 
 		this._prototypeAll();
+
 		this._linkAll();
 	}
 
-	/**
-	 * Checks and updates module info
-	 * @param original Module info to update
-	 * @param updated Updated module info from discovery
-	 * @throws Throws an error if the names are different, which is a sign of invalid function usage
-	 */
-	private _checkAndUpdate(original: Interfaces.IModuleInfo, updated: Interfaces.IModuleInfo) {
-		if (original.name !== updated.name) {
-			throw new Error("The names are different and cannot be updated, the new module must be introduced");
-		}
+	// /**
+	//  * Checks and updates module info
+	//  * @param original Module info to update
+	//  * @param updated Updated module info from discovery
+	//  * @throws Throws an error if the names are different, which is a sign of invalid function usage
+	//  */
+	// private _checkAndUpdate(original: Interfaces.IModuleInfo, updated: Interfaces.IModuleInfo) {
+	// 	if (original.name !== updated.name) {
+	// 		throw new Error("The names are different and cannot be updated, the new module must be introduced");
+	// 	}
 
-		original.main = updated.main;
-		original.version = updated.version;
+	// 	original.main = updated.main;
+	// 	original.version = updated.version;
 
-		for (const name in original.dependencies) {
-			if (!updated.dependencies[name]) {
-				delete original.dependencies[name];
-			}
-		}
+	// 	for (const name in original.dependencies) {
+	// 		if (!updated.dependencies[name]) {
+	// 			delete original.dependencies[name];
+	// 		}
+	// 	}
 
-		for (const name in updated.dependencies) {
-			original.dependencies[name] = updated.dependencies[name];
-		}
-	}
+	// 	for (const name in updated.dependencies) {
+	// 		original.dependencies[name] = updated.dependencies[name];
+	// 	}
+	// }
 
 	/**
 	 * Makes prototypes out of dependencies
@@ -179,14 +255,27 @@ export class ModuleLoader extends EventEmitter {
 
 		let keepersPrototyped = 0;
 
+		const allKeepers = this._allKeepers;
+		const infoMapped = this._infoMappedKeepers;
+
 		for (const name in this._infos) {
 			if (this._keepers[name]) continue;
 
-			const modInfo = this._infos[name]!;
+			const modInfos = this._infos[name]!;
 
-			const modKeeper = new ModuleKeeper(modInfo);
+			const keepers = this._keepers[name] || (this._keepers[name] = []);
 
-			this._keepers[name] = modKeeper;
+			for (let i = 0, l = modInfos.length; i < l; i++) {
+				const mod = modInfos[i];
+			
+				const modKeeper = new ModuleKeeper(mod);
+
+				keepers.push(modKeeper);
+
+				allKeepers.push(modKeeper);
+
+				infoMapped.set(mod, modKeeper);
+			}
 
 			keepersPrototyped++;
 		}
@@ -204,52 +293,66 @@ export class ModuleLoader extends EventEmitter {
 		let logStr = "";
 
 		for (const modName in this._infos) {
-			const modInfo = this._infos[modName]!;
-			const modKeeper = this._getKeeper(modName);
+			const modKeepers = this._getKeepers(modName);
 
-			logStr += `\n\t${modName}@${modInfo.version}`;
+			for (let i = 0, l = modKeepers.length; i < l; i++) {
+				const modKeeper = modKeepers[i];
+				const modInfo = modKeeper.info;
+			
+				logStr += `\n\t${modName}@${modInfo.version}`;
 
-			for (const depName in modInfo.dependencies) {
-				let verRange = modInfo.dependencies[depName]!;
+				for (const depName in modInfo.dependencies) {
+					let verRange = modInfo.dependencies[depName]!;
 
-				const optional = verRange.endsWith("?");
-				if (optional) verRange = verRange.slice(0, -1);
+					const optional = verRange.endsWith("?");
+					if (optional) verRange = verRange.slice(0, -1);
 
-				const depInfo = this._infos[depName]!;
+					const depInfo = this._infos[depName]!;
 
-				if (!depInfo) {
-					if (!optional) {
-						throw new Error(`Dependency "${depName}" cannot be found`);
+					if (!depInfo) {
+						if (!optional) {
+							throw new Error(`Dependency "${depName}" cannot be found`);
+						}
+
+						this._log("info", `Optional: Module "${modName}" requested "${depName}", but it cannot be found`);
+
+						logStr += `\n\t  - MISSING: ${depName}: not found`;
+
+						continue;
 					}
 
-					this._log("info", `Optional: Module "${modName}" requested "${depName}" but it cannot be found`);
+					// Dependencies version map must be built to avoid another loop iteration
+					const depVerMap: IHashMap<Interfaces.IModuleInfo> = Object.create(null);
 
-					logStr += `\n\t  - MISSING: ${depName}: not found`;
+					// Mapping the versions and in the same time adding them to the depVerMap
+					const depVersions = depInfo.map(_ => (depVerMap[_.version] = _, _.version));
 
-					continue;
-				}
+					const satisfyingVer = semver.maxSatisfying(depVersions, verRange);
 
-				const depVersion = depInfo.version;
+					if (!satisfyingVer) {
+						if (!optional) {
+							throw new Error(`Unable to satisfy requirement for "${depName}" of dependent "${modName}": ${verRange} (the only available are: ${depVersions.join()})`);
+						}
 
-				if (!semver.satisfies(depVersion, verRange)) {
-					if (!optional) {
-						throw new Error(`Unable to satisfy requirement for "${depName}" of dependent "${modName}": ${verRange}`);
+						this._log("info", `Optional: Module "${modName}" requested "${depName}" but version no version in set { ${depVersions.join()} } is able to satisfy requirement of "${verRange}"`);
+
+						logStr += `\n\t  - MISSING: ${depName}: version mismatch ("${verRange}" required)`;
+
+						continue;
 					}
 
-					this._log("info", `Optional: Module "${modName}" requested "${depName}" but version "${depVersion}" unable to satisfy requirement of "${verRange}"`);
+					this._log("verb", `Linking "${depName}" to "${modName}": ${satisfyingVer} satisfies ${verRange}`);
 
-					logStr += `\n\t  - MISSING: ${depName}: version mismatch ("${depVersion}" vs "${verRange}")`;
+					const depKeeper = this._infoMappedKeepers.get(depVerMap[satisfyingVer]);
 
-					continue;
+					if (depKeeper == null) {
+						throw new Error(`Invalid state: tried to get keeper whose information matches tho one that was mapped by the version ("${satisfyingVer}"), but got nothing`);
+					}
+
+					modKeeper.addDependency(depKeeper);
+
+					logStr += `\n\t  - ${depName}@${depKeeper.info.version}`;
 				}
-
-				this._log("verb", `Linking "${depName}" to "${modName}": ${depInfo.version} satisfies ${verRange}`);
-
-				const depKeeper = this._getKeeper(depName);
-
-				modKeeper.addDependency(depKeeper);
-
-				logStr += `\n\t  - ${depName}@${depInfo.version}`;
 			}
 		}
 
@@ -267,9 +370,11 @@ export class ModuleLoader extends EventEmitter {
 
 		let constructedCount = 0;
 
-		for (const name in this._keepers) {
-			const keeper = this._getKeeper(name);
+		const allKeepers = this._allKeepers;
 
+		for (let i = 0, l = allKeepers.length; i < l; i++) {
+			const keeper = allKeepers[i];
+		
 			if (keeper.state !== Interfaces.ModuleLoadState.Prototype) {
 				continue;
 			}
@@ -277,6 +382,8 @@ export class ModuleLoader extends EventEmitter {
 			try {
 				await keeper.construct();
 			} catch (err) {
+				const { name } = keeper.info;
+
 				this._log("err", `Unable to construct module "${name}":`, err);
 
 				throw new Error(`Module "${name}" cannot be constructed. See logs for details`);
@@ -301,9 +408,11 @@ export class ModuleLoader extends EventEmitter {
 
 		let initializedCount = 0;
 
-		for (const name in this._keepers) {
-			const keeper = this._getKeeper(name);
+		const allKeepers = this._allKeepers;
 
+		for (let i = 0, l = allKeepers.length; i < l; i++) {
+			const keeper = allKeepers[i];
+		
 			if (keeper.state !== Interfaces.ModuleLoadState.Constructed) {
 				continue;
 			}
@@ -311,6 +420,8 @@ export class ModuleLoader extends EventEmitter {
 			try {
 				await keeper.initialize();
 			} catch (err) {
+				const { name } = keeper.info;
+
 				this._log("err", `Unable to initialize module "${name}":`, err);
 
 				throw new Error(`Module "${name}" cannot be initialized. See logs for details`);
@@ -335,8 +446,10 @@ export class ModuleLoader extends EventEmitter {
 
 		let unloadedCount = 0;
 
-		for (const name in this._keepers) {
-			const keeper = this._getKeeper(name);
+		const allKeepers = this._allKeepers;
+
+		for (let i = 0, l = allKeepers.length; i < l; i++) {	
+			const keeper = allKeepers[i];
 
 			if (keeper.state !== Interfaces.ModuleLoadState.Initialized) {
 				continue;
@@ -353,11 +466,44 @@ export class ModuleLoader extends EventEmitter {
 	}
 
 	/**
-	 * Searches for the keeper by given name
+	 * Searches for the keepers by given name
 	 * @param name Name to look up
 	 */
-	public getKeeper<T>(name: string) {
-		return <ModuleKeeper<T>> this._keepers[name] || undefined;
+	public getKeepers<T>(name: string) {
+		return <Array<ModuleKeeper<T>> | undefined> this._keepers[name];
+	}
+
+	/**
+	 * Searches for a keeper by given name and that matches
+	 * the version range specified, highest version returned
+	 * @param name Name to look up
+	 * @param range Version range
+	 * @example
+	 * $modLoader.getKeeper("my-module", ">=0.6.X")
+	 * // => ModuleKeeper<"my-module", "0.6.3">
+	 */
+	public getKeeper<T>(name: string, range: string) {
+		const keepers = this.getKeepers<T>(name);
+
+		if (!keepers) return undefined;
+
+		let highestSatisfying: ModuleKeeper<T> | null = null;
+
+		for (let i = 0, l = keepers.length; i < l; i++) {
+			const keeper = keepers[i];
+		
+			const { version } = keeper.info;
+
+			if (!semver.satisfies(version, range)) continue;
+
+			if (highestSatisfying != null) {
+				if (semver.gt(highestSatisfying.info.version, version)) continue;
+			}
+
+			highestSatisfying = keeper;
+		}
+
+		return highestSatisfying;
 	}
 
 	/**
@@ -365,12 +511,12 @@ export class ModuleLoader extends EventEmitter {
 	 * @param name Name to look up
 	 * @throws Throws an error if keeper is not found
 	 */
-	private _getKeeper(name: string) {
-		const keeper = this._keepers[name];
+	private _getKeepers(name: string) {
+		const keepers = this._keepers[name];
 
-		if (!keeper) throw new Error(`No keeper found for "${name}"`);
+		if (!keepers) throw new Error(`No keepers found for "${name}"`);
 
-		return keeper;
+		return keepers;
 	}
 }
 
